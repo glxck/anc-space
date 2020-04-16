4f70fde670bef8bc1b9c4b8a0fa978b60ce04345
glxck <dim4ik3013@gmail.com>
2020-04-17 02:58:47 +0300

    Fix z-index and add some markup

--- src/pages/Homepage/Structure.js
+++ src/pages/Homepage/Structure.js
@@ -5,9 +5,15 @@ const Structure = () => {
     return (
         <div className = 'structure'>
             <img src={ BG }/>
-            <div className = 'text'>
+            <div className = 'info'>
+                <h2>Структура компании</h2>
+                <p>
+                    На предприятии
+                    работают более 100 высококвалифицированных сотрудников,
+                    имеющих большой практический опыт в области космической деятельности.
+                </p>
+                <button>УЗНАТЬ БОЛЬШЕ</button>
             </div>
-            <button>УЗНАТЬ БОЛЬШЕ</button>
         </div>
     )
 }
