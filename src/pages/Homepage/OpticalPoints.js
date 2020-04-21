5457f38fd0ffa878abed08646af982786cc54fcc
glxck <dim4ik3013@gmail.com>
2020-04-21 17:37:57 +0300

    Add pics to OpticalPoinst block

--- src/pages/Homepage/OpticalPoints.js
+++ src/pages/Homepage/OpticalPoints.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import LeftPic from '../../img/IMG_PointsLeft.png'
+import RightPic from '../../img/IMG_PointsRight.png'
 
 const OpticalPoints = () => {
     return (
         <div className = 'points'>
             <h2>Оптические пункты наблюдения</h2>
             <div className = 'items'>
-                <div className = 'firstPic'></div>
-                <div className = 'secondPic'></div>
+                <div className = 'firstPic'>
+                    <img src = { LeftPic } />
+                    <p>Гора Шадмахадз</p>
+                </div>
+                <div className = 'secondPic'>
+                    <img src = { RightPic } />
+                    <p>Благовещенский Район</p>
+                </div>
             </div>
         </div>
     )
