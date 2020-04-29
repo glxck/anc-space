6d13e034a7d75f8582e226b18bcd92221043bc25
glxck <dim4ik3013@gmail.com>
2020-04-29 21:44:55 +0300

    Add to gh-pages

---
+++ build/precache-manifest.7ddd34e070a24dd96ca395e497313586.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ade641077565741f1bf30549fbb15bd",
+    "url": "/anc-space/index.html"
+  },
+  {
+    "revision": "3fc7c2977781f6440d7d",
+    "url": "/anc-space/static/css/main.78d810de.chunk.css"
+  },
+  {
+    "revision": "2d41aebc96e729e0a53a",
+    "url": "/anc-space/static/js/2.0ebbf333.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/anc-space/static/js/2.0ebbf333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc7c2977781f6440d7d",
+    "url": "/anc-space/static/js/main.e097881e.chunk.js"
+  },
+  {
+    "revision": "eaacea240f409767b762",
+    "url": "/anc-space/static/js/runtime-main.51cafde2.js"
+  },
+  {
+    "revision": "36be4b22e8096ea0ad1928c85df57bcb",
+    "url": "/anc-space/static/media/BG-Structure.36be4b22.png"
+  },
+  {
+    "revision": "95f4990ee3308fdd72680b231f2cd40d",
+    "url": "/anc-space/static/media/BG_Telescopes.95f4990e.png"
+  },
+  {
+    "revision": "2e3f98dcac1072773afb1d93f0875c42",
+    "url": "/anc-space/static/media/IMG_ANC.2e3f98dc.png"
+  },
+  {
+    "revision": "677d0d49929f39cab9f4010e7ecb36ca",
+    "url": "/anc-space/static/media/IMG_Earth.677d0d49.png"
+  },
+  {
+    "revision": "38b2a5a263f98a50de20206068b64802",
+    "url": "/anc-space/static/media/IMG_PointsLeft.38b2a5a2.png"
+  },
+  {
+    "revision": "52e4a81718ca6dd4fc1961c5d7e21b81",
+    "url": "/anc-space/static/media/IMG_PointsRight.52e4a817.png"
+  }
+]);
