16ab8e8413c40f2656d3623a3905554638704876
glxck <dim4ik3013@gmail.com>
2020-05-08 05:37:58 +0300

    Add molules to package.json and create build folder

---
+++ build/precache-manifest.0b2905b9815a9401fd2664e9a9348c3a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c759f8fc98df84865a3e19f51f004fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0734c3c7671416aca16",
+    "url": "./static/css/main.beab17d6.chunk.css"
+  },
+  {
+    "revision": "3ae3c1933a3d606a76b9",
+    "url": "./static/js/2.897b6582.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.897b6582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0734c3c7671416aca16",
+    "url": "./static/js/main.cafe3600.chunk.js"
+  },
+  {
+    "revision": "93993894c93bd960efb7",
+    "url": "./static/js/runtime-main.c5e9b72e.js"
+  },
+  {
+    "revision": "36be4b22e8096ea0ad1928c85df57bcb",
+    "url": "./static/media/BG-Structure.36be4b22.png"
+  },
+  {
+    "revision": "95f4990ee3308fdd72680b231f2cd40d",
+    "url": "./static/media/BG_Telescopes.95f4990e.png"
+  },
+  {
+    "revision": "b0790e95dec0f5d95f05a36f1688f301",
+    "url": "./static/media/IMG-FirstTel.b0790e95.png"
+  },
+  {
+    "revision": "2e3f98dcac1072773afb1d93f0875c42",
+    "url": "./static/media/IMG_ANC.2e3f98dc.png"
+  },
+  {
+    "revision": "677d0d49929f39cab9f4010e7ecb36ca",
+    "url": "./static/media/IMG_Earth.677d0d49.png"
+  },
+  {
+    "revision": "38b2a5a263f98a50de20206068b64802",
+    "url": "./static/media/IMG_PointsLeft.38b2a5a2.png"
+  },
+  {
+    "revision": "52e4a81718ca6dd4fc1961c5d7e21b81",
+    "url": "./static/media/IMG_PointsRight.52e4a817.png"
+  }
+]);
